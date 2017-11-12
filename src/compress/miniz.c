533111953338904b93cebfdd0ee46257f891066f
Paul Brossier <piem@piem.org>
2017-11-12 22:14:35 +0100

    src/compress/miniz.c: avoid dereferencing type-punned pointer [g++-6]

--- src/compress/miniz.c
+++ src/compress/miniz.c
@@ -2270,7 +2270,8 @@ static MZ_FORCEINLINE void tdefl_find_match(tdefl_compressor *d, mz_uint lookahe
   mz_uint dist = 0, pos = lookahead_pos & TDEFL_LZ_DICT_SIZE_MASK, match_len = *pMatch_len, probe_pos = pos, next_probe_pos, probe_len;
   mz_uint num_probes_left = d->m_max_probes[match_len >= 32];
   const mz_uint16 *s = (const mz_uint16*)(d->m_dict + pos), *p, *q;
-  mz_uint16 c01 = TDEFL_READ_UNALIGNED_WORD(&d->m_dict[pos + match_len - 1]), s01 = TDEFL_READ_UNALIGNED_WORD(s);
+  mz_uint8 *c01_src = &d->m_dict[pos + match_len - 1];
+  mz_uint16 c01 = TDEFL_READ_UNALIGNED_WORD(c01_src), s01 = TDEFL_READ_UNALIGNED_WORD(s);
   MZ_ASSERT(max_match_len <= TDEFL_MAX_MATCH_LEN); if (max_match_len <= match_len) return;
   for ( ; ; )
   {
@@ -2278,10 +2279,13 @@ static MZ_FORCEINLINE void tdefl_find_match(tdefl_compressor *d, mz_uint lookahe
     {
       if (--num_probes_left == 0) return;
       #define TDEFL_PROBE \
+      { \
         next_probe_pos = d->m_next[probe_pos]; \
         if ((!next_probe_pos) || ((dist = (mz_uint16)(lookahead_pos - next_probe_pos)) > max_dist)) return; \
         probe_pos = next_probe_pos & TDEFL_LZ_DICT_SIZE_MASK; \
-        if (TDEFL_READ_UNALIGNED_WORD(&d->m_dict[probe_pos + match_len - 1]) == c01) break;
+        mz_uint8 *c01_check = &d->m_dict[probe_pos + match_len - 1]; \
+        if (TDEFL_READ_UNALIGNED_WORD(c01_check) == c01) break; \
+      }
       TDEFL_PROBE; TDEFL_PROBE; TDEFL_PROBE;
     }
     if (!dist) break;
@@ -2297,7 +2301,8 @@ static MZ_FORCEINLINE void tdefl_find_match(tdefl_compressor *d, mz_uint lookahe
     else if ((probe_len = ((mz_uint)(p - s) * 2) + (mz_uint)(*(const mz_uint8*)p == *(const mz_uint8*)q)) > match_len)
     {
       *pMatch_dist = dist; if ((*pMatch_len = match_len = MZ_MIN(max_match_len, probe_len)) == max_match_len) break;
-      c01 = TDEFL_READ_UNALIGNED_WORD(&d->m_dict[pos + match_len - 1]);
+      mz_uint8 *c01_check = &d->m_dict[probe_pos + match_len - 1];
+      c01 = TDEFL_READ_UNALIGNED_WORD(c01_check);
     }
   }
 }
